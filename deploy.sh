338ed1334a7464270665ad12692ce5b629c4cc05
Jeroen van Vianen <jeroen@vanvianen.nl>
2021-05-07 15:32:48 +0200

    Update Gradle dependencies

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.0.0
+VERSION=1.0.1
 (cd osb; ../gradlew clean build publishToMavenLocal)
 mkdir -p ../onesecondbefore.com/public/repository/com/onesecondbefore/tracker/tracker-android/${VERSION}
 cp ~/.m2/repository/com/onesecondbefore/tracker/tracker-android/${VERSION}/tracker-android-${VERSION}.* ../onesecondbefore.com/public/repository/com/onesecondbefore/tracker/tracker-android/${VERSION}
