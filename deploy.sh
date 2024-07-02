599d2429cc1c88eb9f9f380f63339f95b4786f8c
Eelco van Kuik <eelco@onesecondbefore.com>
2024-07-02 11:39:38 +0200

    no message

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 export JAVA_HOME=$(/usr/libexec/java_home 17)
 
-VERSION=7.1
+VERSION=7.4
 (cd osb; ../gradlew clean build)
 (cd osb; ../gradlew publishToMavenLocal)
 mkdir -p ../onesecondbefore.com/public/repository/com/onesecondbefore/tracker/tracker-android/${VERSION}
