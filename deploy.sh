3fe95da37def86fcff9bccf6502a5ceb4bafb13c
Eelco van Kuik <eelco@onesecondbefore.com>
2024-07-23 16:38:50 +0200

    Removed VERSION from deploy file

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
 
-VERSION=7.4
 (cd osb; ../gradlew clean build)
 (cd osb; ../gradlew publishToMavenLocal)
 mkdir -p ../onesecondbefore.com/public/repository/com/onesecondbefore/tracker/tracker-android/${VERSION}
